174a91b38aadcdac7e4135610115740301139f94
buct1705 <66016920+buct1705@users.noreply.github.com>
2021-01-29 05:37:49 +0800

    Update report.py

--- report.py
+++ report.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
         },
     }
 
-    with open(filename, 'r', encoding='gbk') as f:
+    with open(filename, 'r', encoding='gb2312') as f:
         reader = csv.reader(f)
         reader.__next__()
         for row in reader:
